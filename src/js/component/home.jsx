8534ddfa1039bea1ca8a7b3f3b4bad6d20be2fb5
Juan Carlos Díaz <169100845+Nxusbets@users.noreply.github.com>
2024-08-30 14:15:13 +0000

    primer cambio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
+// Incluye imágenes en tu bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+    const [inputValue, setInputValue] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    return (
+        <div className="container">
+            <h1>My to do list</h1>
+            <ul>
+                <li>
+                    <input 
+                        type="text"
+                        onChange={(e) => setInputValue(e.target.value)}
+                        value={inputValue}
+                        onKeyPress={(e) => {
+                            if (e.key === "Enter") {
+                                setTodos(todos.concat([inputValue]));
+                                setInputValue("");
+                            }
+                        }}
+                        placeholder="What do you need to do?"
+                    />
+                </li>
+                {todos.map((item, index) => (
+                    <li key={index}>
+                        {item} 
+                        <i 
+                            className="fas fa-trash-alt"
+                            onClick={() => setTodos(
+                                todos.filter((_, currentIndex) => index !== currentIndex)
+                            )}
+                        ></i>
+                    </li>
+                ))}
+            </ul>
+            <div>{todos.length} tasks left</div>
+        </div>
+    );
+}
 
 export default Home;
